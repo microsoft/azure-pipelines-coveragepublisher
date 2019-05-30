9e604eba3b9e51122265fd380028dfd21e38f86f
siddhap <siddhap@microsoft.com>
2019-05-30 12:24:23 +0530

    PR review

--- src/CoveragePublisher.L0.Tests/SimpleTimerTests.cs
+++ src/CoveragePublisher.L0.Tests/SimpleTimerTests.cs
@@ -37,7 +37,7 @@ namespace CoveragePublisher.L0.Tests
         [TestMethod]
         public void SimpleTimeWhenOutSideThreshold()
         {
-            var verboseLogCalled = false;
+            var warningLogCalled = false;
             var logger = new Mock<TraceLogger>(new TextWriterTraceListener());
             var clientFactory = new Mock<ClientFactory>(null);
             var telemetryDataCollector = new Mock<TelemetryDataCollector>(clientFactory.Object, logger.Object);
@@ -45,12 +45,14 @@ namespace CoveragePublisher.L0.Tests
             clientFactory
                 .Setup(x => x.GetClient<CustomerIntelligenceHttpClient>())
                 .Returns((CustomerIntelligenceHttpClient)null);
-            logger.Setup(x => x.Verbose(It.IsAny<string>())).Callback((() => verboseLogCalled = true));
+            logger.Setup(x => x.Warning(It.IsAny<string>())).Callback((() => warningLogCalled = true));
             telemetryDataCollector.Setup(x =>
                 x.PublishTelemetryAsync(It.IsAny<string>(), It.IsAny<Dictionary<string, object>>()));
 
             using (new SimpleTimer("testTimer", "Test", "test",
                 logger.Object, telemetryDataCollector.Object, TimeSpan.FromMilliseconds(0), true)) ;
+
+            Assert.IsTrue(warningLogCalled);
         }
 
         [TestMethod]
