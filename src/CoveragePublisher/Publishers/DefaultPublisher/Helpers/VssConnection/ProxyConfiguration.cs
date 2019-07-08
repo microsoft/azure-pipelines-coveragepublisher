4032f51d3baf36cf753cbfdc49cebd7d4592e94b
Karan Jit Singh <karsin@microsoft.com>
2019-07-08 15:37:43 +0530

    Connection proxy (#28)

---
+++ src/CoveragePublisher/Publishers/DefaultPublisher/Helpers/VssConnection/ProxyConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.Pipelines.CoveragePublisher.Publishers.DefaultPublisher
+{
+    internal class ProxyConfiguration
+    {
+        private static ProxyConfiguration _proxyInstance;
+        private static readonly object SyncLock = new Object();
+
+        public string ProxyUrl { get; private set; }
+
+        public string ProxyUserName { get; private set; }
+
+        public string ProxyPassword { get; private set; }
+
+        public string ProxyBypassList { get; private set; }
+
+        protected ProxyConfiguration(string proxyUrl, string proxyUserName, string proxyPassword, string proxyBypassList)
+        {
+            ProxyUrl = proxyUrl;
+            ProxyUserName = proxyUserName;
+            ProxyPassword = proxyPassword;
+            ProxyBypassList = proxyBypassList;
+        }
+        
+        public static ProxyConfiguration GetProxyDetails()
+        {
+            if(_proxyInstance == null)
+            {
+                _proxyInstance = new ProxyConfiguration(Environment.GetEnvironmentVariable(Constants.EnvironmentVariables.ProxyConfiguration.ProxyUrl),
+                                     Environment.GetEnvironmentVariable(Constants.EnvironmentVariables.ProxyConfiguration.ProxyUserName),
+                                     Environment.GetEnvironmentVariable(Constants.EnvironmentVariables.ProxyConfiguration.ProxyPassword),
+                                     Environment.GetEnvironmentVariable(Constants.EnvironmentVariables.ProxyConfiguration.ProxyByPassList));
+            }
+
+            return _proxyInstance;
+        }
+    }
+}
---
+++ src/CoveragePublisher/Publishers/DefaultPublisher/Helpers/VssConnection/VssProxyHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Microsoft.Azure.Pipelines.CoveragePublisher.Publishers.DefaultPublisher
+{
+    internal class VssProxyHelper
+    {
+        public static WebProxy GetProxy()
+        {
+            WebProxy proxy = null;
+
+            if (ProxyConfiguration.GetProxyDetails() == null)
+            {
+                return proxy;
+            }
+
+            if (!string.IsNullOrWhiteSpace(ProxyConfiguration.GetProxyDetails().ProxyUrl)
+                && !string.Equals("undefined", ProxyConfiguration.GetProxyDetails().ProxyUrl, StringComparison.OrdinalIgnoreCase)
+                && !string.Equals("null", ProxyConfiguration.GetProxyDetails().ProxyUrl, StringComparison.OrdinalIgnoreCase))
+            {
+                proxy = new WebProxy(ProxyConfiguration.GetProxyDetails().ProxyUrl, true);
+
+                if (!string.IsNullOrWhiteSpace(ProxyConfiguration.GetProxyDetails().ProxyUserName)
+                    && !string.Equals("undefined", ProxyConfiguration.GetProxyDetails().ProxyUserName, StringComparison.OrdinalIgnoreCase)
+                    && !string.Equals("null", ProxyConfiguration.GetProxyDetails().ProxyUserName, StringComparison.OrdinalIgnoreCase)
+                    && !string.IsNullOrWhiteSpace(ProxyConfiguration.GetProxyDetails().ProxyPassword)
+                    && !string.Equals("undefined", ProxyConfiguration.GetProxyDetails().ProxyPassword, StringComparison.OrdinalIgnoreCase)
+                    && !string.Equals("null", ProxyConfiguration.GetProxyDetails().ProxyPassword, StringComparison.OrdinalIgnoreCase))
+                {
+                    proxy.Credentials = new NetworkCredential(ProxyConfiguration.GetProxyDetails().ProxyUserName, ProxyConfiguration.GetProxyDetails().ProxyPassword);
+                }
+
+                if (!string.IsNullOrWhiteSpace(ProxyConfiguration.GetProxyDetails().ProxyBypassList)
+                    && !string.Equals("undefined", ProxyConfiguration.GetProxyDetails().ProxyBypassList, StringComparison.OrdinalIgnoreCase)
+                    && !string.Equals("null", ProxyConfiguration.GetProxyDetails().ProxyBypassList, StringComparison.OrdinalIgnoreCase))
+                {
+                    proxy.BypassList = JsonConvert.DeserializeObject<string[]>(ProxyConfiguration.GetProxyDetails().ProxyBypassList);
+                }
+
+                proxy.BypassProxyOnLocal = false;
+            }
+
+            return proxy;
+        }
+    }
+}
