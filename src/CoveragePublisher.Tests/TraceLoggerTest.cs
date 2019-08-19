ce2d463976d02c3992d57bf09a24729376717afd
Karan Jit Singh <karsin@microsoft.com>
2019-08-19 13:19:52 +0530

    copyright notices

--- src/CoveragePublisher.Tests/TraceLoggerTest.cs
+++ src/CoveragePublisher.Tests/TraceLoggerTest.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Diagnostics;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TraceLogger = Microsoft.Azure.Pipelines.CoveragePublisher.TraceLogger;
 
 namespace CoveragePublisher.Tests
@@ -58,3 +60,4 @@ namespace CoveragePublisher.Tests
         }
     }
 }
+ 
--- src/CoveragePublisher/Parsers/Parser.cs
+++ src/CoveragePublisher/Parsers/Parser.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using Microsoft.Azure.Pipelines.CoveragePublisher.Model;
 using Microsoft.Azure.Pipelines.CoveragePublisher.Utils;
