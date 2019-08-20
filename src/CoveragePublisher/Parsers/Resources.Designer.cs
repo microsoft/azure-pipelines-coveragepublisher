933545f429bcb9c49271a22ca34b107a1cc86ffe
Karan Jit Singh <karsin@microsoft.com>
2019-08-20 14:59:37 +0530

    logging fix (#35)

--- src/CoveragePublisher/Parsers/Resources.Designer.cs
+++ src/CoveragePublisher/Parsers/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Azure.Pipelines.CoveragePublisher.Parsers {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -61,7 +61,7 @@ namespace Microsoft.Azure.Pipelines.CoveragePublisher.Parsers {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Error occured while generating HTML report. Error message: {0}..
+        ///   Looks up a localized string similar to Error occured while generating HTML report. {0}.
         /// </summary>
         internal static string HTMLReportError {
             get {
