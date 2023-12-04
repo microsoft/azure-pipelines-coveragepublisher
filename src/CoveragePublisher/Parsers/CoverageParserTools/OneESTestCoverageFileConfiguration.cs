fabd8baa63f22b4e285f4851fee5cfeb7eb6f467
Vinayak Joshi (from Dev Box) <vinayakjoshi@microsoft.com>
2023-12-04 12:44:26 +0530

    Adding conversion logics

---
+++ src/CoveragePublisher/Parsers/CoverageParserTools/OneESTestCoverageFileConfiguration.cs
@@ -0,0 +1,53 @@
+// Copyright (C) Microsoft Corporation. All Rights Reserved.
+
+using Microsoft.Azure.Pipelines.CoveragePublisher.Model;
+using Microsoft.TeamFoundation.TestManagement.WebApi;
+using Palmmedia.ReportGenerator.Core;
+using Palmmedia.ReportGenerator.Core.CodeAnalysis;
+using Palmmedia.ReportGenerator.Core.Parser;
+using Palmmedia.ReportGenerator.Core.Parser.Filtering;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+
+namespace Microsoft.Azure.Pipelines.CoveragePublisher.Parsers
+{
+    public class OneESTestCoverageFileConfiguration
+    {
+        /// <inheritdoc/>
+        public bool ReadModules { get; set; }
+
+        /// <inheritdoc/>
+        public bool ReadSkippedModules { get; set; }
+
+        /// <inheritdoc/>
+        public bool ReadSkippedFunctions { get; set; }
+
+        /// <inheritdoc/>
+        public bool ReadSnapshotsData { get; set; }
+
+        /// <inheritdoc/>
+        public bool GenerateCoverageBufferFiles { get; set; }
+
+        /// <inheritdoc/>
+        public bool FixCoverageBuffersMismatch { get; set; }
+
+        /// <inheritdoc/>
+        public int MaxDegreeOfParallelism { get; set; } = 10;
+
+        public bool SkipInvalidData { get; set; }
+
+        internal static OneESTestCoverageFileConfiguration Default { get; } = new OneESTestCoverageFileConfiguration
+        {
+            ReadModules = true,
+            ReadSkippedFunctions = true,
+            ReadSnapshotsData = true,
+            ReadSkippedModules = true,
+            GenerateCoverageBufferFiles = true,
+            FixCoverageBuffersMismatch = true,
+        };
+        
+    }
+}
