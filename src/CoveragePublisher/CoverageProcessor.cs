a5e3f9ea1861c589088e7328a72b5fff3b37c82e
Vinayak Joshi (from Dev Box) <vinayakjoshi@microsoft.com>
2024-11-26 18:48:22 +0530

    dont pulish html for nativecoverage files

--- src/CoveragePublisher/CoverageProcessor.cs
+++ src/CoveragePublisher/CoverageProcessor.cs
@@ -115,9 +115,19 @@ namespace Microsoft.Azure.Pipelines.CoveragePublisher
 
                         else
                         {
-                            using (new SimpleTimer("CoverageProcesser", "PublishHTMLReport", _telemetry))
+                            foreach(string nativeCoverageFile in config.CoverageFiles)
                             {
-                                await _publisher.PublishHTMLReport(config.ReportDirectory, token);
+                                if (!(nativeCoverageFile.EndsWith(Constants.CoverageConstants.CoverageBufferFileExtension) ||
+                                    nativeCoverageFile.EndsWith(Constants.CoverageConstants.CoverageFileExtension) ||
+                                    nativeCoverageFile.EndsWith(Constants.CoverageConstants.CoverageBFileExtension) ||
+                                    nativeCoverageFile.EndsWith(Constants.CoverageConstants.CoverageJsonFileExtension) ||
+                                    nativeCoverageFile.EndsWith(Constants.CoverageConstants.CoverageXFileExtension)))
+                                {
+                                    using (new SimpleTimer("CoverageProcesser", "PublishHTMLReport", _telemetry))
+                                    {
+                                        await _publisher.PublishHTMLReport(config.ReportDirectory, token);
+                                    }
+                                }
                             }
                         }
                     }
